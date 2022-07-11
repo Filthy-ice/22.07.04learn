9f22f7ed542c6abaefc74c3da4b132b55e922086
Filthy-ice <345134522@qq.com>
2022-07-11 21:58:19 +0800

    07.11up to date.

--- test.c
+++ test.c
@@ -3,6 +3,7 @@
 #include<stdio.h>
 #include<assert.h>
 #include<string.h>
+#include<math.h>
 
 //第一题
 //struct Test
@@ -57,28 +58,150 @@
 
 
 //写一个逆序函数让数组中的内容逆序排列。
-void reverse(char* arr)
+//void reverse(char* arr)
+//{
+//	assert(arr);
+//	int len = strlen(arr);
+//	char* left = arr;
+//	char* reight = arr + len - 1;
+//	while (left < reight)
+//	{
+//		char tmp = *left;
+//		*left = *reight;
+//		*reight = tmp;
+//		left++;
+//		reight--;
+//	}
+//	return 0;
+//}
+//
+//int main()
+//{
+//	char arr[256];
+//	scanf("%s", arr);
+//	reverse(arr);
+//	printf("%s\n", arr);
+//	return 0;
+//}
+
+//如Sn=a+aa+aaa+aaaa+aaaaa,求一个数的前五项之和。
+//int main()
+//{
+//	int a = 0;
+//	int n = 0;
+//	int i = 0;
+//	int sum = 0;
+//	int ret = 0;
+//	scanf("%d%d", &a, &n);
+//	for (i = 0; i < n; i++)
+//	{
+//		ret = ret * 10 + a;
+//		sum += ret;
+//	}
+//	printf("%d\n", sum);
+//	return 0;
+//}
+
+//求0~10000之间的水仙花数。如：153=1^3+5^3+3^3,
+//int main()
+//{
+//	int i = 0;
+//	for (i = 0; i < 100000; i++)
+//	{
+//		//1.求未知的位数n。
+//		int n = 1;
+//		int tmp = i;
+//		int sum = 0;
+//		while (tmp /= 10)//tmp越变越小，直至while循环里的值小于10。
+//		{
+//			n++;
+//		}
+//        //2.计算i的每一位n次方之和。
+//		tmp = i;
+//		while (tmp)
+//		{
+//			sum += pow(tmp % 10, n);
+//			tmp /= 10;
+//		}
+//        //3.对比是否符合i==sum。
+//		if (i == sum)
+//		{
+//			printf("%d ", i);
+//		}
+//	}
+//	return 0;
+//}
+
+//写一个程序打印菱形排布的*。
+
+//      *
+//     ***
+//    *****
+//   *******
+//  *********
+// ***********
+//*************
+// ***********
+//  *********
+//   *******
+//    *****
+//     ***
+//      *
+
+//先用老师所用的纯打印方法编写
+//int main()
+//{
+//	//打印上半部分
+//	int line = 0;
+//	scanf("%d", &line);
+//	int i = 0;
+//	for (i = 0; i < line; i++)
+//	{
+//		int j = 0;
+//		//打印空格
+//		for (j = 0; j < line-1-i; j++)
+//		{
+//			printf(" ");
+//		}
+//		for (j = 0; j < 2*i+1; j++)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//	//打印下半部分
+//
+//	for (i = 0; i < line; i++)
+//	{
+//		int j = 0;
+//		//打印空格
+//		for (j = 0; j <= i; j++)
+//		{
+//			printf(" ");
+//		}
+//		for (j = 0; j < 2 * ( line-1-i )-1; j++)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+//用二维数组编写。
+void arrange(char* str)
 {
-	assert(arr);
-	int len = strlen(arr);
-	char* left = arr;
-	char* reight = arr + len - 1;
-	while (left < reight)
-	{
-		char tmp = *left;
-		*left = *reight;
-		*reight = tmp;
-		left++;
-		reight--;
-	}
-	return 0;
+	//已有二维字符数组
+	//1.每一行
+	//2.每一行字符改变从中间向两端递进
+	//3.达到最大值后，从两端向中间递减
+	//4.打印字符数组
+	
 }
 
 int main()
 {
-	char arr[256];
-	scanf("%s", arr);
-	reverse(arr);
-	printf("%s\n", arr);
+	char arr[13][13] = { 0 };
+	arrange(arr);
 	return 0;
 }
